0f19913b488a411f9e4750a59cffdd6c46224df5
diptoroy <diptoroy54@gmail.com>
2020-06-10 18:20:54 +0600

    RegisterActivity xml

--- app/src/main/java/com/atcampus/chasabad/Activity/MainActivity.java
+++ app/src/main/java/com/atcampus/chasabad/Activity/MainActivity.java
@@ -3,14 +3,19 @@ package com.atcampus.chasabad.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.atcampus.chasabad.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/com/atcampus/chasabad/Activity/RegistrationActivity.java
+++ app/src/main/java/com/atcampus/chasabad/Activity/RegistrationActivity.java
@@ -2,15 +2,22 @@ package com.atcampus.chasabad.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.atcampus.chasabad.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    private TextView title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        title = findViewById(R.id.title);
+
     }
 }
