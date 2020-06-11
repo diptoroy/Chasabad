14674ecfb1b17bf9c2922567a3fe7c0efc2b8e95
diptoroy <diptoroy54@gmail.com>
2020-06-11 23:42:20 +0600

    signin fragment

--- app/src/main/java/com/atcampus/chasabad/Activity/RegistrationActivity.java
+++ app/src/main/java/com/atcampus/chasabad/Activity/RegistrationActivity.java
@@ -1,16 +1,25 @@
 package com.atcampus.chasabad.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
+import com.atcampus.chasabad.Fragment.SignInFragment;
+import com.atcampus.chasabad.Fragment.SignUpFragment;
 import com.atcampus.chasabad.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private TextView title;
+    private FrameLayout frameLayout;
+    private RadioButton signInBtn,signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,39 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
 
         title = findViewById(R.id.title);
+        frameLayout = findViewById(R.id.registerFrame);
+        signInBtn = findViewById(R.id.signInBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        setDefaultFragment(new SignInFragment());
 
     }
+
+    public void setDefaultFragment(Fragment fragment){
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void radioButtonChecked(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.signInBtn:
+                if (checked)
+                    setDefaultFragment(new SignInFragment());
+                break;
+            case R.id.signUpBtn:
+                if (checked)
+                    setFragment(new SignUpFragment());
+                    break;
+        }
+        }
+
+
 }
--- app/src/main/java/com/atcampus/chasabad/Fragment/SignInFragment.java
+++ app/src/main/java/com/atcampus/chasabad/Fragment/SignInFragment.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.atcampus.chasabad.R;
 
@@ -16,6 +20,10 @@ public class SignInFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private FrameLayout parentFrameLayout;
+    private EditText email,password;
+    private TextView forgotPassword;
+    private Button signInBtn;
 
 
     @Override
@@ -28,6 +36,14 @@ public class SignInFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+        parentFrameLayout = getActivity().findViewById(R.id.registerFrame);
+        email = view.findViewById(R.id.mailEditText);
+        password = view.findViewById(R.id.passwordEditText);
+        forgotPassword = view.findViewById(R.id.fogotPasswordtextView);
+        signInBtn = view.findViewById(R.id.signInBtn);
+
+        return view;
     }
 }
