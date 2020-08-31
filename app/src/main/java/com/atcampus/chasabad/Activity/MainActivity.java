888604af2e033465f0e2784b3e8fc73aa46e427f
diptoroy <diptoroy54@gmail.com>
2020-08-31 11:33:30 +0600

    location

--- app/src/main/java/com/atcampus/chasabad/Activity/MainActivity.java
+++ app/src/main/java/com/atcampus/chasabad/Activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.atcampus.chasabad.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -9,21 +10,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.ResultReceiver;
+import android.provider.Settings;
 import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.atcampus.chasabad.Adapter.MenuAdapter;
 import com.atcampus.chasabad.Adapter.TipsAdapter;
-import com.atcampus.chasabad.AppService.FetchIntentAddressService;
-import com.atcampus.chasabad.AppService.LocationConstants;
 import com.atcampus.chasabad.Model.MenuModel;
 import com.atcampus.chasabad.Model.TipsModel;
 import com.atcampus.chasabad.R;
@@ -34,15 +42,17 @@ import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+import static androidx.constraintlayout.motion.widget.Debug.getLocation;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
 
     private RecyclerView menuRecyclerView, tipsRecyclerView;
     private TextView locationTextView;
 
-    private final static int REQUEST_CODE_LOCATION_PERMISSION = 1;
-    private ResultReceiver resultReceiver;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
         locationTextView = findViewById(R.id.locationText);
         LocationData();
-        resultReceiver = new addressResultReceiver(new Handler());
 
         menuRecyclerView = findViewById(R.id.menu_recyclerView);
         menuRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
@@ -78,94 +87,95 @@ public class MainActivity extends AppCompatActivity {
         TipsAdapter tipsAdapter = new TipsAdapter(tipsModelList);
         tipsRecyclerView.setAdapter(tipsAdapter);
         tipsAdapter.notifyDataSetChanged();
+        
+        //Location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationEnabled();
+        getLocation();
     }
 
-    private void LocationData() {
-        if (ContextCompat.checkSelfPermission(
-                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION
-        ) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_CODE_LOCATION_PERMISSION
-            );
-        } else {
-            getCurrentLocation();
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 500, 5, (LocationListener) this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void locationEnabled() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (!gps_enabled && !network_enabled) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Enable GPS Service")
+                    .setMessage("We need your GPS location to show Near Places around you.")
+                    .setCancelable(false)
+                    .setPositiveButton("Enable", new
+                            DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                                    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                                }
+                            })
+                    .setNegativeButton("Cancel", null)
+                    .show();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        try {
+            Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            locationTextView.setText(addresses.get(0).getLocality() + addresses.get(0).getLocality());
+//            tvState.setText(addresses.get(0).getAdminArea());
+//            tvCountry.setText(addresses.get(0).getCountryName());
+//            tvPin.setText(addresses.get(0).getPostalCode());
+//            locationTextView.setText(addresses.get(0).getAddressLine(0));
+
+        } catch (Exception e) {
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
-            getCurrentLocation();
-        } else {
-            Toast.makeText(this, "permission Denied!", Toast.LENGTH_SHORT).show();
-        }
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
     }
 
-    private void getCurrentLocation() {
+    @Override
+    public void onProviderEnabled(String provider) {
 
-        final LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(1000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        LocationServices.getFusedLocationProviderClient(MainActivity.this)
-                .requestLocationUpdates(locationRequest, new LocationCallback() {
-                    @Override
-                    public void onLocationResult(LocationResult locationResult) {
-                        super.onLocationResult(locationResult);
-                        LocationServices.getFusedLocationProviderClient(MainActivity.this)
-                                .removeLocationUpdates(this);
-                        if (locationResult != null && locationResult.getLocations().size() > 0) {
-                            int latestLocation = locationResult.getLocations().size() - 1;
-                            double latitude = locationResult.getLocations().get(latestLocation).getLatitude();
-                            double longitude = locationResult.getLocations().get(latestLocation).getLatitude();
-                            locationTextView.setText(String.format("lat %s\nlong %s",latitude,longitude));
-                            Location location = new Location("providerNA");
-                            location.setLatitude(latitude);
-                            location.setLongitude(longitude);
-                            fetchAddressFromLocation(location);
-                        }
-
-                    }
-                }, Looper.getMainLooper());
     }
 
-    private void fetchAddressFromLocation(Location location){
-        Intent intent = new Intent(this, FetchIntentAddressService.class);
-        intent.putExtra(LocationConstants.RECEIVER,resultReceiver);
-        intent.putExtra(LocationConstants.LOCATION_DATA_EXTRA,location);
-        startService(intent);
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
-    private class addressResultReceiver extends ResultReceiver {
-
-        addressResultReceiver(Handler handler) {
-            super(handler);
+    private void LocationData() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
         }
 
-        @Override
-        protected void onReceiveResult(int resultCode, Bundle resultData) {
-            super.onReceiveResult(resultCode, resultData);
-            if (resultCode == LocationConstants.SUCCESS_RESULT){
-                //locationTextView.setText(resultData.getString(LocationConstants.RESULT_DATA_KEY));
-            }else{
-                Toast.makeText(MainActivity.this,resultData.getString(LocationConstants.RESULT_DATA_KEY),Toast.LENGTH_LONG).show();
-            }
-        }
     }
+
+
+
 }
